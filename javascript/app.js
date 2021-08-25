d00a9b8fe5891bdf65f54d5c5ef7f3917c090f9f
erikLinn <htein.linn210@gmail.com>
2021-08-25 15:29:46 +0630

    third

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+function show() {
+  var userName = "Mg Mg";
+  document.write("Hello JS");
+  console.log("Hello JS");
+  alert("Hello JS");
+  // document.getElementById("hello").innerHTML = "DOM Hello JS";
+  document.write(` <br> ${userName} lives in 'Yangon'`);
+}
